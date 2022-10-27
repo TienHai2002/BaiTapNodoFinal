d7e90110210c83134dc7819eea5661e2a0f50c56
TienHai2002 <volibear25102501@gmail.com>
2022-10-27 10:05:58 +0700

    xong import export nhung chua hien len table

--- angular-training/src/app/pages/bigproject/danhmuc/danhmuc.component.ts
+++ angular-training/src/app/pages/bigproject/danhmuc/danhmuc.component.ts
@@ -6,8 +6,8 @@ import {DanhMucService} from '../../../shared/services/danhmuc.service';
 import {LoaidanhmucService} from "../../../shared/services/loaidanhmuc.service";
 import Swal from 'sweetalert2';
 import {MatTableDataSource} from "@angular/material/table";
-
-
+import * as XLSX from 'xlsx';
+type AOA = any[][];
 @Component({
     selector: 'app-danhmuc',
     templateUrl: './danhmuc.component.html',
@@ -20,6 +20,8 @@ export class DanhmucComponent implements OnInit {
     items = [];
     itemDM: any;
     page = new Page();
+
+    data: AOA = [[],[]];
     columns = [
         {name: 'STT', prop: 'index', flexGrow: 1.5},
         {name: 'MaDanhMuc', prop: 'maDm', flexGrow: 1.5},
@@ -196,5 +198,39 @@ export class DanhmucComponent implements OnInit {
     // getDMbyTen(ma: number) {
     //    return this.itemDM.filter((x: { id: number; }) => x.id == ma);
     // }
+    wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+    fileName: string = 'SheetJS.xlsx';
+    onFileChange(evt: any) {
+        /* wire up file reader */
+        const target: DataTransfer = <DataTransfer>(evt.target);
+        if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+        const reader: FileReader = new FileReader();
+        reader.onload = (e: any) => {
+            /* read workbook */
+            const bstr: string = e.target.result;
+            const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
+            /* grab first sheet */
+            const wsname: string = wb.SheetNames[0];
+            const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+            /* save data */
+            this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+            console.log(this.data);
+        };
+        reader.readAsBinaryString(target.files[0]);
+    }
+
+
+    export(): void {
+        /* generate worksheet */
+        const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+        /* generate workbook and add the worksheet */
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+        /* save to file */
+        XLSX.writeFile(wb, this.fileName);
+    }
 }
--- angular-training/src/app/pages/bigproject/loaidanhmuc/loaidanhmuc.component.ts
+++ angular-training/src/app/pages/bigproject/loaidanhmuc/loaidanhmuc.component.ts
@@ -6,6 +6,9 @@ import Swal from "sweetalert2";
 import {LoaidanhmucService} from "../../../shared/services/loaidanhmuc.service";
 import {DanhMucService} from 'src/app/shared/services/danhmuc.service';
 import {MatDialog} from '@angular/material/dialog';
+import * as XLSX from "xlsx";
+type AOA = any[][];
+type AOALDM = any[][];
 
 @Component({
     selector: 'app-loaidanhmuc',
@@ -19,6 +22,8 @@ export class LoaidanhmucComponent implements OnInit {
     page = new Page();
     page1 = new Page();
 
+    data: AOA = [[],[]];
+    dataLDM : AOALDM = [[],[]];
     columns = [
         {name: 'STT', prop: 'index', flexGrow: 1.5},
         {name: 'Mã', prop: 'ma', flexGrow: 3},
@@ -312,5 +317,40 @@ export class LoaidanhmucComponent implements OnInit {
     }
     clickRowTable(item: any) {
         this.itemsDM = item.itemDM;
+    }
+    wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+    fileName: string = 'SheetJS.xlsx';
+    onFileChange(evt: any) {
+        /* wire up file reader */
+        const target: DataTransfer = <DataTransfer>(evt.target);
+        if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+        const reader: FileReader = new FileReader();
+        reader.onload = (e: any) => {
+            /* read workbook */
+            const bstr: string = e.target.result;
+            const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+            /* grab first sheet */
+            const wsname: string = wb.SheetNames[0];
+            const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+            /* save data */
+            this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+            console.log(this.data);
+        };
+        reader.readAsBinaryString(target.files[0]);
+    }
+
+
+    export(): void {
+        /* generate worksheet */
+        const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+        /* generate workbook and add the worksheet */
+        const wb: XLSX.WorkBook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+        /* save to file */
+        XLSX.writeFile(wb, this.fileName);
     }
 }
